7febc8d65724e36e3d455630b6c6dc5c6dda3dd7
MarcioSM101 <marcio-home@MacBook-Pro-de-Marcio.local>
2024-04-24 00:43:20 -0400

    second commit

---
+++ assets/js/funciones.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // Desplazamiento suave al hacer clic en los enlaces de la barra de navegación
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
