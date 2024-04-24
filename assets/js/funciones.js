7d071735be75a20708c3ea769d1d2d46f17ff691
MarcioSM101 <marcio-home@MacBook-Pro-de-Marcio.local>
2024-04-24 01:32:02 -0400

    fourth commit

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,23 +1,30 @@
-$(document).ready(function(){
-    // Desplazamiento suave al hacer clic en los enlaces de la barra de navegación
+$(document).ready(function() {
     $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
-        if( target.length ) {
+        if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
             }, 1000);
         }
     });
-        $(window).scroll(function() {    
-            var scroll = $(window).scrollTop();
-            if (scroll > 50) {
-                $(".navbar").css("background-color", "#333"); // Cambiar el color de fondo al hacer scroll
-            } else {
-                $(".navbar").css("background-color", "transparent"); // Restaurar el color de fondo al llegar al principio
-            }
-        });
-    
-});
-    
 
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        if (scroll > 50) {
+            $(".navbar").css("background-color", "#333");
+        } else {
+            $(".navbar").css("background-color", "transparent");
+        }
+        $('.parallax').css('top', -(scroll * 0.2) + 'px');
+    });
+
+    $('.navbar-nav .nav-link').click(function(){
+        var $enlace = $(this);
+        $enlace.css('font-size', '20px');
+        setTimeout(function(){
+            $enlace.css('color', ''); 
+            $enlace.css('font-size', ''); 
+        }, 1000);
+    });
+});
